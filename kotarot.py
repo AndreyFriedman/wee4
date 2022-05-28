24d11c07343bd4cbb378c5f7aa88c3813e33072b
AndreyFriedman <74827765+AndreyFriedman@users.noreply.github.com>
2022-05-29 01:00:55 +0300

    Add files via upload

---
+++ kotarot.py
@@ -0,0 +1,16 @@
+def schedule_display(sol):
+    # this function is only going to get the solution (or the starting table before the algorithm)
+    # and print it on the screen
+    return
+
+
+def cost_func(sol):
+    # this function is going to check if the given solution by the simulator is good enough
+    # it will check if exist a better solution
+    return
+
+
+def simulator(solu):
+    # this function is going to solve the problem resource algorithm
+    # it will use the cost_func to compare the new result with the previous result (the upper bound) and 0 (lower bound)
+    return
